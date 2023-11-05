7d74e87a48d489b65e97b231b0547d70ca4089d7
Misube <mishataube2005@gmail.com>
2023-11-05 23:49:30 +0200

    lab_1 copied to lab_2

---
+++ lab_2/image_handler.py
@@ -0,0 +1,131 @@
+import numpy
+import cv2
+import copy
+
+class ImageContainer:
+    def __init__(self,path=None):
+        if path != None: #if path is defined, import the image
+            self.path = path
+            self.data = cv2.imread(path)
+    path = None
+    data = []
+
+    def show(self): #show the image and wait for anykey
+        cv2.imshow(self.path, self.data)
+        cv2.waitKey(0)
+
+    def save(self):
+        cv2.imwrite(self.path, self.data)
+
+def IC_to_monochrome(input_IC): #create monochromatic copy of an image
+    if type(input_IC.data[0][0]) != list or not isinstance(input_IC,ImageContainer):
+        Exception("IC_to_monochrome accepts only rgb image containers")
+    output_IC=copy.deepcopy(input_IC) #create copy of an image
+    output_IC.path=None #delete a path of a copy
+
+    pos_y = 0
+    for row in output_IC.data:
+        pos_x = 0
+        for pixel in row:
+            output_IC.data[pos_y][pos_x] = int(pixel[0]*0.36+pixel[1]*0.53+pixel[2]*0.11) #monochromatic transformation
+            pos_x+=1
+        pos_y+=1
+
+    return output_IC #return a monochromatic copy
+
+def monochrome_to_binary(input_IC):
+    if type(input_IC.data[0][0])!=int or not isinstance(input_IC,ImageContainer):
+        Exception("monochrome_to_binary accepts only monochromatic image containers")
+    output_IC = copy.deepcopy(input_IC)  # create copy of an image
+    output_IC.path = None  # delete a path of a copy
+
+    if True:
+        brightness_graph = [0]*256
+        for row in input_IC.data:
+            for pixel in row:
+                brightness_graph[pixel[0]] +=1
+
+        total_brightness = sum(brightness_graph)
+
+        brightness_possibility = [0]*256
+        for i in range(0,255):
+            brightness_possibility[i] = brightness_graph[i]/total_brightness
+
+        def q1(t):
+            output = sum(brightness_possibility[0:t])
+            if output != 0:
+                return output
+            else:
+                return 0.0001
+
+        def q2(t):
+            output = sum(brightness_possibility[t+1:])
+            if output != 0:
+                return output
+            else:
+                return 0.0001
+
+        def mu1(t):
+            output = 0
+            for i in range(0, t):
+                output += i * brightness_possibility[i] / q1(t)
+            return output
+
+        def mu2(t):
+            output = 0
+            for i in range(t+1,255):
+                output += i*brightness_possibility[i]/q2(t)
+            return output
+
+        def main_disp(t):
+            return q1(t)*q2(t)*((mu1(t)*mu2(t))**2)
+
+        max_val = -10**10
+        t_max = 0
+        for t in range(0,255):
+            value = main_disp(t)
+            if value > max_val:
+                t_max = t
+                max_val = value
+        pos_y = 0
+        for row in output_IC.data:
+            pos_x = 0
+            for pixel in row:
+                if pixel[0]>t_max:
+                    output_IC.data[pos_y][pos_x] = 255
+                else:
+                    output_IC.data[pos_y][pos_x] = 0
+                pos_x += 1
+            pos_y += 1
+
+        return output_IC
+
+
+def mask_cut(mask_IC, input_IC, negative = False):
+    if type(mask_IC.data[0][0])!=int or not isinstance(input_IC,ImageContainer) or not isinstance(mask_IC,ImageContainer):
+        Exception("mask_cut accepts only binary image containers for masks and image containers for input_IC")
+
+    if mask_IC.data.size != input_IC.data.size:
+        Exception("mask must have the same size and shape as the image")
+    output_IC = copy.deepcopy(input_IC)  # create copy of an image
+    output_IC.path = None  # delete a path of a copy
+
+    if negative:
+        pos_y = 0
+        for row in input_IC.data:
+            pos_x = 0
+            for pixel in row:
+                if (mask_IC.data[pos_y][pos_x] == 255).all():
+                    output_IC.data[pos_y][pos_x] = [0,0,0]
+                pos_x += 1
+            pos_y += 1
+    else:
+        pos_y = 0
+        for row in input_IC.data:
+            pos_x = 0
+            for pixel in row:
+                if (mask_IC.data[pos_y][pos_x] == 0).all():
+                    output_IC.data[pos_y][pos_x] = [0,0,0]
+                pos_x += 1
+            pos_y += 1
+    return output_IC
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,1 +1,22 @@
-pass
+import numpy
+import cv2
+import image_handler as IH
+
+while True:
+    Image = IH.ImageContainer(input("relative path: ")) #import the image
+    print("Please wait. Operations may take a while")
+    Image_monochrome = IH.IC_to_monochrome(Image) #monochromatic copy of the image
+    Image_binary = IH.monochrome_to_binary(Image_monochrome) #binary mask of the image
+    Image_mask = IH.mask_cut(Image_binary,Image, negative=True) #cutting out the mask
+
+    Image_monochrome.show() #show monochromatic image
+    Image_binary.show() #show binary mask
+    Image_mask.show() #sow cut image
+    Image.show() #show that original image is untouched
+
+    if input("Do you wish to save masked picture? (Y/N) ") in ["Yes","yes","Y","y"]:
+        cv2.destroyAllWindows()
+        Image_mask.path = input("save path: ")
+        Image_mask.save()
+    else:
+        cv2.destroyAllWindows()
