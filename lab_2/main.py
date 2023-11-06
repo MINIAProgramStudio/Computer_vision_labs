c574e8370decd456c1ffc073768dc8968eb4894b
Misube <mishataube2005@gmail.com>
2023-11-06 02:30:36 +0200

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -4,7 +4,7 @@ import numpy
 
 while True:
     Image = IH.ImageContainer(input("Relative path: ")) #import the image
-    Filter = IH.Convolution_filter([
+    Gauss_11_Filter = IH.Convolution_filter([
         [1, 2, 4, 8, 16, 32, 16, 8, 4, 2, 1],
         [2, 4, 8, 16, 32, 64, 32, 8, 4, 2, 1],
         [4, 8, 16, 32, 64, 128, 64, 32, 16, 8, 4],
@@ -15,7 +15,7 @@ while True:
         [2, 4, 8, 16, 32, 64, 32, 8, 4, 2, 1],
         [1, 2, 4, 8, 16, 32, 16, 8, 4, 2, 1]
     ])
-    Image_filtered = Filter.apply(Image)
+    Image_filtered = Gauss_11_Filter.apply(Image)
     Image_filtered.standardize()
     Image_filtered.show()#show filtered image
     #cv2.destroyAllWindows()
