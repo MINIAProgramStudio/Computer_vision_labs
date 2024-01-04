342d46c353473f60f911a78f76acd23d3c9c7d14
Misube <mishataube2005@gmail.com>
2024-01-04 13:59:21 +0200

    slat-pepper noise generation introduced

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,2 +1,5 @@
-import image_handler
-import convolution_filters_presets
+import image_handler as IH
+import convolution_filters_presets as CFP
+
+le_image = IH.ImageContainer("input/1.png")
+IH.sp_noise(le_image).show()
